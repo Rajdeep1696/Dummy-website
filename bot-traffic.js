2d6e81ecbf98e6ca8dbcb5f27ebdfd6924e85914
Rajdeep1696 <Rajdeep604.rs@gmail.com>
2024-09-15 17:55:21 +0530

    Create bot-traffic.js

---
+++ bot-traffic.js
@@ -0,0 +1,13 @@
+// Function to simulate bot visits
+function simulateBotTraffic() {
+    const totalVisits = 10; // Number of simulated visits
+    
+    for (let i = 0; i < totalVisits; i++) {
+        // Simulate a visit to your website
+        console.log("Bot visited your website!");
+        // Here, you could also add some logic to track the visits
+    }
+}
+
+// Run the function to start simulating bot traffic
+simulateBotTraffic();
